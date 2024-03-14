3caf0b4367be1c523ca79c288036d5398bc64422
Rein de Guzman <37677855+spartanrein@users.noreply.github.com>
2024-03-14 09:34:53 +0800

    change score schema

--- catcher-backend/models/scoreModel.js
+++ catcher-backend/models/scoreModel.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 const ScoreSchema = new Schema({
     playerName: {type: String, required: true},
-    score: {type: Int32, required:true}
+    score: {type: Number, required:true}
 },{timestamps:true})
 
 const Score = mongoose.model('Score', ScoreSchema)
