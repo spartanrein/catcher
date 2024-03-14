6c5f8c6b2ddf414fcccc479151e13e22ed9f966a
Rein de Guzman <37677855+spartanrein@users.noreply.github.com>
2024-03-14 08:46:10 +0800

    add scoreModel

---
+++ catcher-backend/models/scoreModel.js
@@ -0,0 +1,12 @@
+const { Int32 } = require('mongodb')
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const ScoreSchema = new Schema({
+    playerName: {type: String, required: true},
+    score: {type: Int32, required:true}
+},{timestamps:true})
+
+const Score = mongoose.model('Score', ScoreSchema)
+
+module.exports = Score
